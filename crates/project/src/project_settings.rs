52a3d6b3c38a72ed2d51f733cf60b070c6a88269
Smit Barmase <heysmitbarmase@gmail.com>
2026-01-08 11:42:24 +0530

    rebase fix

--- crates/project/src/project_settings.rs
+++ crates/project/src/project_settings.rs
@@ -1280,8 +1280,7 @@ impl SettingsObserver {
                 store
                     .editorconfig_settings
                     .set_external_paths_for_worktree(worktree_id, external_paths);
-            })
-            .ok();
+            });
         })
         .detach();
     }
